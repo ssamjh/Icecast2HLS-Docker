40451776a302aaf237f6386f5474885d6085809f
ssamjh <sam.howell.sjh@gmail.com>
2023-09-14 13:10:24 +1200

    Reduce buffer

--- reconnect.sh
+++ reconnect.sh
@@ -5,7 +5,7 @@ INPUT_URL=${INPUT_URL}
 OUTPUT_URL=${OUTPUT_URL}
 
 while true; do
-    ffmpeg -re -hide_banner -i $INPUT_URL -c copy -bsf:a aac_adtstoasc -f flv $OUTPUT_URL -loglevel error
+    ffmpeg -re -hide_banner -i $INPUT_URL -c copy -bsf:a aac_adtstoasc -max_muxing_queue_size 400 -flush_packets 1 -f flv $OUTPUT_URL -loglevel error
     echo "Stream went down. Reconnecting in 30 seconds..."
     sleep 30
 done
